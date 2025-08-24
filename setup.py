b56d4d7275131fc2f8f5b44a38b7b9bce4049a43
Richard Hopkins <hopkira@googlemail.com>
2025-08-24 22:11:24 +0100

    Fixes

--- setup.py
+++ setup.py
@@ -1,34 +1,34 @@
-from setuptools import setup
-import os
-from glob import glob
+from setuptools import setup, find_packages
 
 package_name = 'k9_chess'
 
 setup(
     name=package_name,
     version='0.1.0',
-    packages=[package_name],
-    data_files=[
-        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        # Include any scripts or BT files
-        (os.path.join('share', package_name), glob('k9_chess/bt/*.py')),
-        (os.path.join('share', package_name), glob('k9_chess/utils/*.py')),
+    packages=find_packages(exclude=['test']),
+    install_requires=[
+        'setuptools',
+        'rclpy',
+        'python-chess',
+        'py_trees',
+        'requests',
     ],
-    install_requires=['setuptools'],
+
     zip_safe=True,
-    author='Richard Hopkins',
-    author_email='richard@example.com',
-    description='ROS 2 Python nodes for K9 chess robot (Lichess + Stockfish)',
-    license='MIT',
-    tests_require=['pytest'],
+    maintainer='Richard Hopkins',
+    maintainer_email='hopkira@gmail.com',
+    description='ROS 2 Python nodes for K9 Chess Robot',
+    license='Apache-2.0',
     entry_points={
         'console_scripts': [
             'game_manager_node = k9_chess.game_manager_node:main',
             'chess_engine_node = k9_chess.chess_engine_node:main',
-            'move_sender_node = k9_chess.move_sender_node:main',
             'chess_state_node = k9_chess.chess_state_node:main',
-            'chess_bt = k9_chess.bt.chess_bt:main',
         ],
     },
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/k9_chess']),
+        ('share/k9_chess/launch', ['launch/chess_launch.py']),
+        ],
 )
+
